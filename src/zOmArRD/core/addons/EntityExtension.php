19ca83c9aef4fd889fab75f9610dd87067fd8e73
zOmAr <64379283+zOmArRD@users.noreply.github.com>
2021-01-26 15:56:04 -0400

    EntityExtension.php added

---
+++ src/zOmArRD/core/addons/EntityExtension.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace zOmArRD\core\addons;
+
+use pocketmine\entity\Entity;
+use pocketmine\entity\Skin;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
+use zOmArRD\core\providers\Entity as API;
+use zOmArRD\core\utils\Npc;
+
+class EntityExtension implements API
+{
+    public function spawnEntity(string $name, float $x, float $y, float $z, Level $level, Player $player): void
+    {
+        foreach ($level->getEntities() as $entity){
+            if ($entity instanceof Npc){
+                if ($entity->getSkin()->getSkinId() === $name){
+                    $entity->kill();
+                }
+            }
+        }
+        $nbt = Entity::createBaseNBT(new Vector3($x, $y, $z));
+        $nbt->setTag(clone $player->namedtag->getCompoundTag("Skin"));
+        $human = new Npc($player->getLevel(), $nbt);
+        $human->setNameTag("§dloading resources...");
+        $human->setNameTagVisible(true);
+        $human->setNameTagAlwaysVisible(true);
+        $human->setSkin(new Skin($name, $player->getSkin()->getSkinData(), $player->getSkin()->getCapeData(), $player->getSkin()->getGeometryName(), $player->getSkin()->getGeometryData()));
+        $human->yaw = $player->getYaw();
+        $human->setScale(1.2);
+        $human->pitch = $player->getPitch();
+        $human->spawnToAll();
+    }
+}
